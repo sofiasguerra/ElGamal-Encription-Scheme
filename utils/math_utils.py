c75cb2f2b83cb7bd799c80f1de643e791c8bb04d
Samuel Feitosa <samuelmatos2007@gmail.com>
2026-07-31 15:42:08 -0300

    assinatura dos métodos em MathUtils.py

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -1,1 +1,19 @@
 #modulo de funções matematicas
+
+class MathUtils:
+
+    @staticmethod
+    def gcd(a: int, b: int) -> int:
+        pass
+
+    @staticmethod
+    def extended_gcd(a: int, b: int):
+        pass
+
+    @staticmethod
+    def modular_inverse(a: int, m: int):
+        pass
+
+    @staticmethod
+    def mod_pow(base: int, exponent: int, modulus: int) -> int:
+        pass
