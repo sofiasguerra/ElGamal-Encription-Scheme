7ce0ed30500dc7088f233863e11a2f9586a594aa
Samuel Feitosa <samuelmatos2007@gmail.com>
2026-08-06 23:07:52 -0300

    add conversoes para base 256

--- utils/message_utils.py
+++ utils/message_utils.py
@@ -1,3 +1,6 @@
+from utils.math_utils import MathUtils
+
+
 class MessageUtils:
     @staticmethod
     def texto_para_bytes(texto):
@@ -65,3 +68,41 @@ class MessageUtils:
         bytes = MessageUtils.numero_para_bytes(numero)
         mensagem = MessageUtils.bytes_para_texto(bytes)
         return mensagem
+
+    @staticmethod
+    def texto_para_blocos(mensagem, p):
+        letras = list(mensagem.encode("utf-8"))
+        blocos = []
+
+        # Agrupar os códigos ASCII em blocos menores que p
+        k = 1
+        while (256 ** (k + 1)) < p:
+            k += 1
+
+        #separar em blocos e transformar para base 256
+        n_em_bloco = 0
+        lista_auxiliar = []
+        for letra in letras:
+            lista_auxiliar.append(letra)
+            n_em_bloco += 1
+
+            if n_em_bloco == k:
+                blocos.append(MathUtils.grupo_para_256(lista_auxiliar))
+                lista_auxiliar = []
+                n_em_bloco = 0
+
+        # Tratar o último bloco se não estiver completo
+        if lista_auxiliar:
+            blocos.append(MathUtils.grupo_para_256(lista_auxiliar))
+
+        return blocos
+
+    @staticmethod
+    def base256_para_texto(lista_base_256):
+        lista_bytes = []
+
+        for bloco in lista_base_256:
+            lista_auxiliar = MathUtils._256_para_grupo(bloco)
+            lista_bytes.extend(lista_auxiliar)
+
+        return bytes(lista_bytes).decode("utf-8")
